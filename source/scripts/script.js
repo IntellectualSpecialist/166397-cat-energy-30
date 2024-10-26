ef6905ae228bf07ebbf061f1d07a3c67aaf414e3
Stepan Vasilyev <intellectualspecialist@gmail.com>
2024-10-26 19:12:01 +0500

    Реализует слайдер + некоторые правки

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,6 +1,11 @@
 const siteList = document.querySelector('.main-nav__list');
 const navBurger = document.querySelector('.main-nav__burger');
 const navMain = document.querySelector('.main-nav');
+const slider = document.querySelector('.slider');
+let control;
+let sliderStyles;
+let controlPlaceStart;
+let clientX;
 
 siteList.classList.add('main-nav__list--closed');
 navBurger.classList.remove('main-nav__burger--active');
@@ -15,3 +20,35 @@ navBurger.addEventListener('click', () => {
     navBurger.classList.remove('main-nav__burger--active');
   }
 });
+
+if (slider) {
+  control = slider.querySelector('.slider__control');
+  sliderStyles = getComputedStyle(control);
+}
+
+function initSlider () {
+  if (slider) {
+    window.addEventListener('pointerup', stopTheControlShifting);
+    control.addEventListener('pointerdown', startTheControlShifting);
+  }
+}
+
+function startTheControlShifting (event) {
+  controlPlaceStart = parseInt(sliderStyles.getPropertyValue('--left'), 10);
+  clientX = event.clientX;
+  window.addEventListener('pointermove', shiftТheСurtain);
+}
+
+function shiftТheСurtain (event) {
+  const deltaX = event.clientX - clientX;
+  const cursorPlace = controlPlaceStart / 100 + deltaX / slider.clientWidth;
+  const controlPlace = Math.min(Math.max(cursorPlace, 0), 1);
+  control.style.setProperty('--left', `${controlPlace * 100}%`);
+  slider.style.setProperty('--persent', `${controlPlace * 100}%`);
+}
+
+function stopTheControlShifting () {
+  window.removeEventListener('pointermove', shiftТheСurtain);
+}
+
+initSlider();
